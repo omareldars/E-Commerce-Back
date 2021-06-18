b3c0b4fcebe78bbee85240546c00af98f9b66e29
omareldars <omar.a.eldars@gmail.com>
2021-06-18 15:38:23 +0200

    Finish Cart

--- routes/cart.js
+++ routes/cart.js
@@ -5,32 +5,25 @@ const auth = require('../middlewares/auth');
 const Product = require('../models/Products');
 
 router.post('/add', auth, (req, res) => {
-  console.log('Order Request', req);
+  // console.log('Order Request', req);
   //   const user = req.user.id;
 
-  //   const products = req.body.products;
+    const products = req.body.products;
 
   // const { body ,} = req;
-  //
-
-  const {
-    body,
-    user: { id },
-  } = req;
-  const cart = new Cart({
-    ...body,
-    user: id,
-  });
-
+  // lets give it a try at first
+  const {body, user: { id },} = req;
+  const cart = new Cart({...body, user: id,});
+  console.log("body---->",body);
+  console.log("req.body--->",req.body.products);
+  console.log("cart--->",cart);
   cart.save((err, data) => {
     if (err) {
       return res.status(400).json({
         error: 'Your request could not be processed. Please try again.',
       });
     }
-
-    decreaseQuantity(body);
-
+    decreaseQuantity(products);
     res.status(200).json({
       success: true,
       cartId: data.id,
@@ -51,17 +44,54 @@ router.delete('/delete/:cartId', auth, (req, res) => {
   });
 });
 
-const decreaseQuantity = (body) => {
-  let bulkOptions = body.map((item) => {
+
+router.post('/add/:cartId', auth, (req, res) => {
+  console.log("from add to cart req--->",req);
+  const product = req.body.product;
+  const query = { _id: req.params.cartId };
+
+  Cart.updateOne(query, { $push: { products: product } }).exec(err => {
+    if (err) {
+      return res.status(400).json({
+        error: 'Your request could not be processed. Please try again.'
+      });
+    }
+    res.status(200).json({
+      success: true
+    });
+  });
+});
+
+router.delete('/delete/:cartId/:productId', auth, (req, res) => {
+  const product = { product: req.params.productId };
+  const query = { _id: req.params.cartId };
+
+  Cart.updateOne(query, { $pull: { products: product } }).exec(err => {
+    if (err) {
+      return res.status(400).json({
+        error: 'Your request could not be processed. Please try again.'
+      });
+    }
+    res.status(200).json({
+      success: true
+    });
+  });
+});
+
+const decreaseQuantity = products => {
+  let bulkOptions = products.map(item => {
     return {
       updateOne: {
         filter: { _id: item.product },
-        update: { $inc: { quantity: -item.quantity } },
-      },
+        update: { $inc: { quantity: -item.quantity } }
+      }
     };
   });
 
   Product.bulkWrite(bulkOptions);
 };
+
+
+
 
 module.exports = router;
